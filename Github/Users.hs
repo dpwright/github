33819fbb5d7e4cd7525f112b1b1f30ddfce668a8
Daniel P. Wright <dani@dpwright.com>
2015-11-22 09:13:57 +0900

    Add a conditional equivalent to userInfoFor'

--- Github/Users.hs
+++ Github/Users.hs
@@ -3,6 +3,7 @@
 module Github.Users (
  userInfoFor
 ,userInfoFor'
+,conditionalUserInfoFor'
 ,userInfoCurrent'
 ,module Github.Data
 ) where
@@ -22,6 +23,13 @@ userInfoFor' auth userName = githubGet' auth ["users", userName]
 -- > userInfoFor "mike-burns"
 userInfoFor :: String -> IO (Either Error DetailedOwner)
 userInfoFor = userInfoFor' Nothing
+
+-- | The information for a single user, by login name.
+-- | With authentification, and conditional on an 'ETag' value.
+--
+-- > conditionalUserInfoFor (Just (GithubOAuth "...")) (Just (ETag "...")) "mike-burns"
+conditionalUserInfoFor' :: Maybe GithubAuth -> Maybe ETag -> String -> IO (Either Error (Maybe ETag, DetailedOwner))
+conditionalUserInfoFor' auth etag userName = githubGetConditional auth etag ["users", userName]
 
 -- | Retrieve information about the user associated with the supplied authentication.
 --
