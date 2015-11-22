74e94c9003e564dd5eab496879c92b71db2f3524
Daniel P. Wright <dani@dpwright.com>
2015-11-22 09:13:15 +0900

    Expose ETag outside of Github.Private

---
+++ Github/Conditional.hs
@@ -0,0 +1,5 @@
+-- | Tools for working with GitHub's "Conditional Requests":
+-- https://developer.github.com/v3/#conditional-requests
+module Github.Conditional (P.ETag(..)) where
+
+import qualified Github.Private as P
